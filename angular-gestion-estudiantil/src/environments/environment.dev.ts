c6bc98c8afdd4238ad5d228a22df0f1627ddc9c5
EdsonMera <86860865+EdsonMera@users.noreply.github.com>
2023-02-07 22:02:58 -0600

    Add files via upload

---
+++ angular-gestion-estudiantil/src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    uri: 'http://localhost:3000/' //api montada en este localhost 
+    //la cambie al puerto 3000 si no fucniona cambiarla al puerto 3000
+    
+  };
